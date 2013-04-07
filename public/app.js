4d92b3cff02bff127bec33ee06931ebeac300305
Carl Jackson <carl@avtok.com>
2013-04-07 00:47:07 -0400

    Some stuff works sometimes

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@
   var twitter = socket.of('/twitter');
   var replay = socket.of('/replay');
 
-  var token;
+  var token, map;
 
   if (!localStorage.credentials) {
     twitter.emit('request_token');
@@ -26,7 +26,6 @@
     e = e.originalEvent; // IDK jQuery
     if (e.key == 'verifier' && e.newValue) {
       var v = JSON.parse(e.newValue);
-      console.log(v);
       if (!token || v.token != token.token) {
         throw new Error("Something mysterious has happened with OAuth");
       }
@@ -38,10 +37,39 @@
     }
   });
 
+  function update_bounds(map) {
+    var bounds = map.getBounds();
+    var ne = bounds.getNorthEast(), sw = bounds.getSouthWest();
+    var round = function (f, n) {
+       return f(n * 1) / 1;
+    }, fl = round.bind(null, Math.floor), ce = round.bind(null, Math.ceil);
+    bounds = [fl(sw.lat()), fl(sw.lng()), ce(ne.lat()), ce(ne.lng())];
+    console.log(bounds);
+    if (localStorage.credentials) {
+      //twitter.emit('stream', {locations: bounds.join(',')});
+      twitter.emit('stream', {locations: '-122.75,36.8,-121.75,37.8'});
+    }
+  }
+
   $(function() {
-    twitter.emit('stream', {locations: '-180,-90,180,90'});
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: new google.maps.LatLng(42.37839,-71.11291),
+      zoom: 12,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    var t = setTimeout(update_bounds.bind(null, map), 500);
+    map.addListener('bounds_changed', function() {
+      if (t) clearTimeout(t);
+      t = setTimeout(update_bounds.bind(null, map), 500);
+    });
     twitter.on('data', function(d) {
-      console.log(d);
+      var c = d.geo.coordinates;
+      marker = new google.maps.Marker({
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP,
+        position: new google.maps.LatLng(c[0], c[1])
+      });
     });
   });
 }();

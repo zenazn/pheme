0ac9d0d45d2398aff89a7cf05bdfe386f5f26b1e
Carl Jackson <carl@avtok.com>
2013-05-02 20:04:28 -0400

    Scrubber thing sort of works

---
+++ public/js/scrubber.js
@@ -0,0 +1,63 @@
+define(['d3'], function(d3) {
+  "use strict";
+
+  var el = $('#scrubber'), brush, data = [];
+
+  function add_data(d) {
+    data.push(d.time);
+  }
+
+  var x = d3.time.scale(), y = d3.scale.linear();
+  var svg = d3.select('#scrubber').append('svg'), g = svg.append('g');
+  var axg = g.append('g'), brg = g.append('g');
+
+  function draw_scrubber() {
+    // Inspiration from http://bl.ocks.org/mbostock/1667367#index.html
+    var margin = {top: 10, right: 10, bottom: 30, left: 10};
+    var width = el.width() - margin.left - margin.right;
+    var height = el.height() - margin.top - margin.bottom;
+
+    svg.attr('width', width + margin.left + margin.right);
+    svg.attr('height', height + margin.top + margin.bottom);
+
+    var hist = d3.layout.histogram()
+      .bins(Math.ceil((d3.max(data) - d3.min(data)) / 10000)) // 10s hunks
+      (data);
+
+    x.range([0, width]).domain(d3.extent(hist.map(function(d) { return d.x })));
+    y.range([height, 0]).domain([0, d3.max(hist, function(d) { return d.y })]);
+
+    var xAxis = d3.svg.axis().scale(x).orient('bottom');
+    //var yAxis = d3.svg.axis().scale(y).orient('left');
+
+    brush = d3.svg.brush().x(x).on('brush', update);
+
+    var area = d3.svg.area()
+      .interpolate('monotone')
+      .x(function(d) { return x(d.x); })
+      .y0(height)
+      .y1(function(d) { return y(d.y); });
+
+    g.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+      .append('path').datum(hist)
+      .attr('d', area);
+
+    axg.attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xAxis);
+
+    brg.attr('class', 'x brush')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(brush);
+
+  }
+
+  function update() {
+    console.log('hi');
+  }
+
+  return {
+    add: add_data,
+    draw: draw_scrubber
+  }
+});
